b43e625a3aa40c74af76ea0377049c67e60fce95
Alexander Austin <Alexander.Austin@blackbaud.me>
2020-12-01 20:45:23 -0500

    Problem 1 solved

---
+++ AdventofCode/BusinessLogic/Day1/ResortRepair.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace AdventofCode.BusinessLogic.Day1
+{
+    /// <summary>
+    /// 2020 Advent of Code Day 1 Problem 1
+    /// </summary>
+    public class ResortRepair
+    {
+        /// <summary>
+        /// ExpenseReport Input
+        /// </summary>
+        private List<int> ExpenseReport { get; set; }
+
+        /// <summary>
+        /// The Sum to be be found from expense report items
+        /// </summary>
+        private int DesiredSum { get; set; }
+
+        public ResortRepair(string filePath, int desiredSum)
+        {
+            ExpenseReport = ReadInputsFromFile(filePath);
+            DesiredSum = desiredSum;
+        }
+
+        /// <summary>
+        /// Returns The Product of the 2 expense report values that sum to 2020
+        /// </summary>
+        /// <returns></returns>
+        public int GetSolution()
+        {
+            int[] multiplicands = new int[2];
+            bool sumFound = false;
+            for (int i = 0; i < ExpenseReport.Count && !sumFound; i++)
+            {
+                for (int j = i; j < ExpenseReport.Count && !sumFound; ++j)
+                {
+                    if (ExpenseReport[i] + ExpenseReport[j] == DesiredSum)
+                    {
+                        multiplicands[0] = ExpenseReport[i];
+                        multiplicands[1] = ExpenseReport[j];
+                        sumFound = true;
+                    }
+                }
+            }
+
+            return multiplicands[0] * multiplicands[1];
+        }
+
+        /// <summary>
+        /// Reads a list of ints from the file at the specified path
+        /// </summary>
+        /// <param name="filepath"></param>
+        /// <returns></returns>
+        private List<int> ReadInputsFromFile(string filepath)
+        {
+            List<int> expenseItems = new List<int>();
+
+            using (TextReader reader = File.OpenText(filepath))
+            {
+                string item;
+                while ((item = reader.ReadLine()) != null)
+                {
+                    expenseItems.Add(int.Parse(item.Trim()));
+                }
+            }
+
+            return expenseItems;
+
+        }
+    }
+}
---
+++ AdventofCode/Program.cs
@@ -0,0 +1,34 @@
+﻿using AdventofCode.BusinessLogic.Day1;
+using System;
+using System.IO;
+
+namespace AdventofCode
+{
+    class Program
+    {
+        /// <summary>
+        /// Prefixes filepath string to navigate to the assets folder
+        /// </summary>
+        /// <returns></returns>
+        public static string ASSET_PATH = "..//..//..//Assets//";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Filename you would like to read from: ");
+            string filename = Console.ReadLine();
+            Console.WriteLine("Enter the desired sum you would like to calculate from: ");
+            string desiredSum = Console.ReadLine();
+            string path = ASSET_PATH + filename;
+
+
+            var resortRepair = new ResortRepair(path, int.Parse(desiredSum));
+
+            var res = resortRepair.GetSolution();
+
+            Console.WriteLine("The sum is: " + res);
+
+           
+        }
+
+    }
+}
